33dbba775c0759d5e08d9d98785572122d4c5d18
SimonHerbst <simon.herbst@sr.htlweiz.at>
2025-05-26 08:45:08 +0000

    beginn

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
+
+  // Exercise 1
+  
+
+
+
+  // Exercise 2
+  int array[10];
+ 
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%d.Zahl: ", i + 1);
+    scanf("%d", &array[i]);
+  }
+
+
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%d ", array[i]);
+
+  }
+    
+  // Exercise 3
+
+
+
+
+
+
 }
