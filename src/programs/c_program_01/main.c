84be87c8e678d238fc42426956f82115aee6f39a
SimonHerbst <simon.herbst@sr.htlweiz.at>
2025-06-02 09:25:20 +0000

    fertig

--- src/programs/c_program_01/main.c
+++ src/programs/c_program_01/main.c
@@ -2,8 +2,40 @@
 
 #include <test_c_lib.h>
 
-int main(int argc, char **argv) {
-  printf("Trying to confirm.\n");
-  say_hello();
+void init(int array[], int length, int value)
+{
+  for (int i = 0; i < length; i++)
+  {
+    array[i] = value;
+  }
+  
+  
+}
+
+
+
+
+int main() 
+{
+  printf("Hello!\n");
+ 
+  // function "init(int array[], int length, int value)"
+  // initialisiere ein Array "array" der Länge 5 mit dem Wert 7
+  int array[5];
+
+  init(array, 5, 7);
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   return 0;
 }
+
